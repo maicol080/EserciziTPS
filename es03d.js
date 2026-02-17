79207cb3cff9d4c500dd439f16cb922b7092eff4
maicol080 <Lucagionno@gmail.com>
2026-02-17 15:05:26 +0100

    Delete es03d.js

--- es03d.js
+++
@@ -1,9 +0,0 @@
-for (let i = 0; i < 3; i++) {
-  console.log("Dentro il ciclo (let): " + i);
-}
-console.log("Fuori dal ciclo (let): " + i);
-
-for (var j = 0; j < 3; j++) {
-  console.log("Dentro il ciclo (var): " + j);
-}
-console.log("Fuori dal ciclo (var): " + i);
