5d061068e7af3a865df436d5f44ad79a8645f178
Michele Scimonelli <lucagionno@gmail.com>
2026-02-17 14:09:18 +0000

    Basi di JavaScript

---
+++ es03d.js
@@ -0,0 +1,9 @@
+for (let i = 0; i < 3; i++) {
+	console.log("Dentro il ciclo (let): " + i);
+}
+console.log("Fuori dal ciclo (let): " + i);
+
+for (var j = 0; i < 3; j++) {
+	console.log("Dentro il ciclo (var): " + j);
+}
+console.log("Fuori dal ciclo (var): " + j
