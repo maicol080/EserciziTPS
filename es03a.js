982a328d4c6d849c7ed27eae67f5f405dc4e90a8
Michele Scimonelli <lucagionno@gmail.com>
2026-02-17 13:46:42 +0000

    Basi di JavaScript

---
+++ es03a.js
@@ -0,0 +1,5 @@
+let larghezza = prompt('Inserire larghezza:');
+let altezza = prompt('Inserire altezza:');
+let lunghezza = prompt('Inserisci lunghezza:');
+let volume = larghezza*altezza*lunghezza;
+console.log('Volume: ' + volume);
