f5ec53ad1c1a70c94bf5d411039b1d4862fbf263
maicol080 <Lucagionno@gmail.com>
2026-02-17 15:00:23 +0100

    Fix console.log indentation in loops

--- es03d.js
+++ es03d.js
@@ -1,9 +1,9 @@
 for (let i = 0; i < 3; i++) {
-ìconsole.log("Dentro il ciclo (let): " + i);
+  console.log("Dentro il ciclo (let): " + i);
 }
 console.log("Fuori dal ciclo (let): " + i);
 
 for (var j = 0; j < 3; j++) {
-ìconsole.log("Dentro il ciclo (var): " + j);
+  console.log("Dentro il ciclo (var): " + j);
 }
 console.log("Fuori dal ciclo (var): " + i);
