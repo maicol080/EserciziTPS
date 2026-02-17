bc0fd9dcac817a3c95968a819a518bc9fb4be1be
Michele Scimonelli <lucagionno@gmail.com>
2026-02-17 14:18:42 +0000

    Basi di JavaScript

---
+++ es03e.js
@@ -0,0 +1,21 @@
+let val1 = 10;
+let val2 = "Ciao";
+let val3 = null;
+let val4 = {};
+let valt = function() {};
+
+if (typeof val1 === "number") {
+	console.log("E' un numero");
+}
+if (typeof val2 === "string") {
+	console.log("E' una stringa");
+}
+if (typeof val3 === "null") {
+	console.log("E' null");
+}
+if (typeof val4 === "object") {
+	console.log("E' un oggetto");
+}
+if (typeof val5 === "function") {
+	console.log("E' una funzione");
+}
